a5d0e1e24374a4502b8b65589234ee8fe3ca5d5a
ragnoril <ozanemirhan@gmail.com>
2023-04-30 01:37:12 +0300

    base movement and stork model added.

--- Assets/Scripts/StorkController.cs
+++ Assets/Scripts/StorkController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class StorkController : CustomBehaviour
 {
@@ -17,6 +18,10 @@ public class StorkController : CustomBehaviour
     private Camera _camera;
     private LineRenderer _lineRenderer;
 
+    [SerializeField] private Vector3 _targetPosition;
+    [SerializeField] private bool _isTurning;
+    [SerializeField] private float _rotateAmount;
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -29,27 +34,44 @@ public class StorkController : CustomBehaviour
         if (!_isMoving) return;
 
         _rigidbody.velocity = transform.up * _moveSpeed * Time.fixedDeltaTime;
+        if (!_isTurning)
+        {
+            _rigidbody.angularVelocity = Vector3.zero;
+        }
+        else
+        {
+            var direction = (transform.position - _targetPosition).normalized;
+
+            _rotateAmount = Vector3.Cross(direction, transform.up).y;
+
+            if ((transform.position - _targetPosition).sqrMagnitude < 1f)
+            {
+                _isTurning = false;
+            }
+            _rigidbody.angularVelocity = transform.forward * -_rotateAmount * (_turnSpeed * Time.fixedDeltaTime);
+        }
         
-        _rigidbody.angularVelocity = transform.forward * _turnSpeed * Time.fixedDeltaTime;
+        
+        
     }
 
     private void Update()
     {
         if (_isMoving) return;
 
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+        
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitData;
-            if (Physics.Raycast(ray, out hitData, 1000))
-            {
-                Vector3 mousePosition = hitData.point;
-                Debug.Log(mousePosition);
-                mousePosition.y = transform.position.y;
-                DrawLine(mousePosition);
-                Debug.Log(mousePosition);
-            }
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15f);
             
+            Vector3 mousePosition = _camera.ScreenToWorldPoint(mousePos);
+
+            mousePosition.y = transform.position.y;
+            DrawLine(mousePosition);
+            _targetPosition = mousePosition;
+            _isTurning = true;
         }
     }
 
@@ -60,7 +82,7 @@ public class StorkController : CustomBehaviour
         var pointList = new List<Vector3>();
 
         Vector3 point1 = transform.position;
-        Vector3 point2 = transform.position + (transform.up * _moveSpeed);
+        Vector3 point2 = transform.position + (transform.up * 3f);
         Vector3 point3 = target;
 
         Debug.Log(point3 + " " + point2 + " " + point1);
